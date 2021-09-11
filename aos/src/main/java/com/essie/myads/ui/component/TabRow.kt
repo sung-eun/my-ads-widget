ddb2cfd69d4d0fe58408a3f7d11288429d633309
sung-eun <sungeunj0@naver.com>
2021-09-11 17:53:52 +0900

    💄 Add Tab row compose

---
+++ aos/src/main/java/com/essie/myads/ui/component/TabRow.kt
@@ -0,0 +1,139 @@
+package com.essie.myads.ui.component
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.*
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.essie.myads.ui.home.HomeScreen
+import com.essie.myads.ui.theme.AppTheme
+
+@Composable
+fun CustomTabRow(
+    allScreens: List<HomeScreen>,
+    onTabSelected: (HomeScreen) -> Unit,
+    currentScreen: HomeScreen
+) {
+    Surface(
+        Modifier
+            .height(TabHeight)
+            .fillMaxWidth()
+    ) {
+        Row(Modifier.selectableGroup()) {
+            allScreens.forEach { screen ->
+                CustomTab(
+                    text = screen.name,
+                    icon = screen.icon,
+                    onSelected = { onTabSelected(screen) },
+                    selected = currentScreen == screen
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun CustomTab(
+    text: String,
+    icon: ImageVector,
+    onSelected: () -> Unit,
+    selected: Boolean
+) {
+    val color = MaterialTheme.colors.onSurface
+    val durationMillis = if (selected) TabFadeInAnimationDuration else TabFadeOutAnimationDuration
+    val animSpec = remember {
+        tween<Color>(
+            durationMillis = durationMillis,
+            easing = LinearEasing,
+            delayMillis = TabFadeInAnimationDelay
+        )
+    }
+
+    val tabTintColor by animateColorAsState(
+        targetValue = if (selected) color else color.copy(alpha = InactiveTabOpacity),
+        animationSpec = animSpec
+    )
+    Row(
+        modifier = Modifier
+            .padding(start = 16.dp, end = 16.dp)
+            .animateContentSize()
+            .height(TabHeight)
+            .selectable(
+                selected = selected,
+                onClick = onSelected,
+                role = Role.Tab,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(
+                    bounded = false,
+                    radius = Dp.Unspecified,
+                    color = Color.Unspecified
+                )
+            )
+            .clearAndSetSemantics { contentDescription = text },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(imageVector = icon, contentDescription = text, tint = tabTintColor)
+        if (selected) {
+            Spacer(Modifier.width(8.dp))
+            Text(
+                modifier = Modifier.wrapContentHeight(),
+                textAlign = TextAlign.Center,
+                text = text,
+                color = tabTintColor,
+                fontSize = 14.sp
+            )
+        }
+    }
+}
+
+private val TabHeight = 56.dp
+private const val InactiveTabOpacity = 0.60f
+
+private const val TabFadeInAnimationDuration = 150
+private const val TabFadeInAnimationDelay = 100
+private const val TabFadeOutAnimationDuration = 100
+
+@Preview
+@Composable
+fun PreviewCustomTab() {
+    AppTheme {
+        Scaffold(
+            topBar = {
+                Surface(
+                    Modifier
+                        .height(TabHeight)
+                        .fillMaxWidth()
+                ) {
+                    Row(Modifier.selectableGroup()) {
+                        CustomTab(
+                            text = "OVERVIEW",
+                            icon = HomeScreen.OVERVIEW.icon,
+                            onSelected = { },
+                            selected = true
+                        )
+                    }
+                }
+            }
+        ) {}
+    }
+}
