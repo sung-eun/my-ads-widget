2a12a51259a98ae1300bf553d29fc65674b060b4
sung-eun <sungeunj0@naver.com>
2021-09-17 00:25:04 +0900

    💄 Add Splash, app icon

---
+++ aos/src/main/java/com/essie/myads/ui/splash/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.essie.myads.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.essie.myads.ui.home.MainActivity
+import kotlinx.coroutines.FlowPreview
+
+@SuppressLint("CustomSplashScreen")
+@FlowPreview
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Intent(this, MainActivity::class.java).also {
+            startActivity(it)
+        }
+    }
+}
