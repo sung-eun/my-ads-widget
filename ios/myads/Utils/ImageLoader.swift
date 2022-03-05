20103b9ea194592d5179ca564918b3c1547cd008
sung-eun <sungeunj0@naver.com>
2022-03-05 11:35:16 +0900

    ⚡️ Add ImageLoader

---
+++ ios/myads/Utils/ImageLoader.swift
@@ -0,0 +1,30 @@
+//
+//  ImageLoader.swift
+//  myads
+//
+//  Created by sung-eun on 2022/03/05.
+//  Copyright © 2022 essie-cho. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class ImageLoader: ObservableObject {
+    var didChange = PassthroughSubject<Data, Never>()
+    var data = Data() {
+        didSet {
+            didChange.send(data)
+        }
+    }
+    
+    init(urlString:String) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }
+        task.resume()
+    }
+}
