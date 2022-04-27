fe7275d750a8eb12b53d574108fc8d2f98e4a275
sung-eun <sungeunj0@naver.com>
2022-04-27 18:21:57 +0900

    ⚡️ Improve some UI/UX

--- ios/myads/Views/GADBannerViewController.swift
+++ ios/myads/Views/GADBannerViewController.swift
@@ -13,24 +13,11 @@ import UIKit
 struct GADBannerViewController: UIViewControllerRepresentable {
 
     func makeUIViewController(context: Context) -> UIViewController {
-        let view = GADBannerView(adSize: GADAdSizeBanner)
+        let view = GADBannerView(adSize: GADCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(UIScreen.main.bounds.size.width))
         let viewController = UIViewController()
         view.adUnitID = "ca-app-pub-2898876693899601/8304589132"
         view.rootViewController = viewController
         viewController.view.addSubview(view)
-        
-        let frame = { () -> CGRect in
-          // Here safe area is taken into account, hence the view frame is used
-          // after the view has been laid out.
-          if #available(iOS 11.0, *) {
-            return view.frame.inset(by: view.safeAreaInsets)
-          } else {
-            return view.frame
-          }
-        }()
-        let viewWidth = frame.size.width
-        view.adSize = GADCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(viewWidth)
-        
         view.load(GADRequest())
         return viewController
     }
